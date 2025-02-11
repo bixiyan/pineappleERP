cf0648f125763bf429b4470d46fc803afd9aa145
bixy2 <bixy2@lenovo.com>
2025-02-11 10:14:07 +0800

    temp commit for new order status report fix bug

--- jshERP-web/src/views/report/OrderStatusReport.vue
+++ jshERP-web/src/views/report/OrderStatusReport.vue
@@ -7,7 +7,7 @@
         <div class="table-page-search-wrapper">
           <a-form layout="inline" @keyup.enter.native="searchQuery">
             <a-row :gutter="24">
-              <a-col :md="6" :sm="24">
+              <a-col :md="4" :sm="24">
                 <a-form-item label="施工状态" :labelCol="labelCol" :wrapperCol="wrapperCol">
                   <a-select placeholder="请选择施工状态" v-model="queryParam.status">
                     <a-select-option value="0">未施工</a-select-option>
@@ -16,7 +16,7 @@
                   </a-select>
                 </a-form-item>
               </a-col>
-              <a-col :md="6" :sm="24">
+              <a-col :md="4" :sm="24">
                 <a-form-item label="设计师" :labelCol="labelCol" :wrapperCol="wrapperCol">
                   <a-select placeholder="请选择设计师" showSearch optionFilterProp="children" v-model="queryParam.designer">
                     <a-select-option v-for="(item,index) in designerList" :key="index" :value="item">
@@ -25,7 +25,7 @@
                   </a-select>
                 </a-form-item>
               </a-col>
-              <a-col :md="6" :sm="24">
+              <a-col :md="4" :sm="24">
                 <a-form-item label="客户" :labelCol="labelCol" :wrapperCol="wrapperCol">
                   <a-select placeholder="请选择客户" v-model="queryParam.organId"
                             :dropdownMatchSelectWidth="false" showSearch allow-clear optionFilterProp="children">
@@ -35,18 +35,8 @@
                   </a-select>
                 </a-form-item>
               </a-col>
-              <a-col :md="6" :sm="24">
-                <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
-                  <a-button type="primary" @click="searchQuery">查询</a-button>
-                  <a-button style="margin-left: 8px" v-print="'#reportPrint'" icon="printer">打印</a-button>
-                  <a-button style="margin-left: 8px" @click="exportExcel" icon="download">导出</a-button>
-                  <a @click="handleToggleSearch" style="margin-left: 8px">
-                    {{ toggleSearchStatus ? '收起' : '展开' }}
-                    <a-icon :type="toggleSearchStatus ? 'up' : 'down'"/>
-                  </a>
-                </span>
-                <template v-if="toggleSearchStatus">
-                  <a-form-item label="单据日期" :labelCol="labelCol" :wrapperCol="wrapperCol">
+              <a-col :md="5" :sm="24">
+                <a-form-item label="单据日期" :labelCol="labelCol" :wrapperCol="wrapperCol">
                     <a-range-picker
                       style="width:100%"
                       v-model="queryParam.createTimeRange"
@@ -56,7 +46,13 @@
                       @ok="onDateOk"
                     />
                   </a-form-item>
-                </template>
+              </a-col>
+              <a-col :md="5" :sm="24">
+                <span class="table-page-search-submitButtons">
+                  <a-button type="primary" @click="searchQuery">查询</a-button>
+                  <a-button style="margin-left: 8px" v-print="'#reportPrint'" icon="printer">打印</a-button>
+                  <a-button style="margin-left: 8px" @click="exportExcel" icon="download">导出</a-button>
+                </span>
               </a-col>
             </a-row>
           </a-form>
@@ -101,6 +97,9 @@
                 <a-icon type="setting" />
               </a-popover>
             </span>
+            <span slot="action" slot-scope="text, record">
+              <a @click="showAccountInOutList(record)">{{record.id?'流水':''}}</a>
+            </span>
           </a-table>
           <a-row :gutter="24" style="margin-top: 8px;text-align:right;">
             <a-col :md="24" :sm="24">
@@ -127,15 +126,12 @@
 </template>
 <script>
   import { JeecgListMixin } from '@/mixins/JeecgListMixin'
-  import { getNowFormatYear, getMpListShort, getPrevMonthFormatDate, getFormatDate } from '@/utils/util'
-  import {getAction} from '@/api/manage'
-  import {findBySelectCus, getAllOrganizationTreeByUser} from '@/api/api'
   import JEllipsis from '@/components/jeecg/JEllipsis'
-  import moment from 'moment'
-  import Vue from 'vue'
+  import {getAction} from '@/api/manage'
+  import {getMaterialNames,findBySelectOrgan} from '@/api/api'
   export default {
     name: "OrderStatusReport",
-    mixins:[JeecgListMixin,FinancialListMixin],
+    mixins:[JeecgListMixin],
     components: {
       JEllipsis
     },
@@ -148,24 +144,21 @@
           span: 18,
           offset: 1
         },
+        // 查询条件
         queryParam: {
-          status:'',
-          designer: undefined,
-          organId: undefined
+          name:'',
+          serialNo:''
         },
         ipagination:{
           pageSize: 11,
           pageSizeOptions: ['11', '21', '31', '101', '201']
         },
-        cusList: [],
-        depotList: [],
-        orgaTree: [],
-        realityPriceTotal: '',
+        allMonthAmount: '',
+        allCurrentAmount: '',
         tabKey: "1",
-        pageName: 'orderStatusReport',
+        pageName: 'OrderStatusReport',
         // 默认索引
-        defDataIndex:['rowIndex','barCode','materialName','materialStandard','materialModel','materialOther','materialUnit',
-          'outSum','outSumPrice','inSum','inSumPrice','outInSumPrice'],
+        defDataIndex:['rowIndex','organName','materialName','paied','unpaied','status'],
         // 默认列
         defColumns: [
           {
@@ -174,103 +167,65 @@
               return (t !== '合计') ? (parseInt(index) + 1) : t
             }
           },
-          {title: '客户', dataIndex: 'organName',width:120, ellipsis:true},
-          {title: '设计师', dataIndex: 'materialName', width: 160, ellipsis:true},
-          {title: '已收款项', dataIndex: 'materialStandard', width: 80, ellipsis:true},
-          {title: '未收款项', dataIndex: 'materialModel', width: 80, ellipsis:true},
-          {title: '施工状态', dataIndex: 'materialColor', width: 60, ellipsis:true},
+          { title: '客户', dataIndex: 'organName', width: 150},
+          { title: '设计师', dataIndex: 'materialName', width: 150},
+          { title: '已收款项', dataIndex: 'paied', width: 100},
+          { title: '未收款项', dataIndex: 'unpaied', width: 100},
+          { title: '施工状态', dataIndex: 'status', width: 100}
         ],
         url: {
           list: "/order/list"
-        }
+        },
+        cusList: [],
+        designerList: []
       }
     },
     created () {
-      this.initCustomer()
-      this.getDepotData()
-      this.loadAllOrgaData()
       this.initColumnsSetting()
       this.initDesigner()
+      this.initOrgan()
     },
     methods: {
-      moment,
-      create(){
+      initDesigner() {
+        getMaterialNames({}).then((res)=>{
+          if(res && res.code === 200) {
+            this.designerList = res.data;
+          }
+        });
+      },
+      initOrgan() {
+        findBySelectOrgan({}).then((res)=>{
+          if(res) {
+            this.cusList = res;
+          }
+        });
       },
       getQueryParams() {
         let param = Object.assign({}, this.queryParam, this.isorter);
-        param.monthTime = this.queryParam.monthTime;
         param.field = this.getQueryField();
         param.currentPage = this.ipagination.current;
         param.pageSize = this.ipagination.pageSize-1;
         return param;
       },
-      loadData(arg) {
-        //加载数据 若传入参数1则加载第一页的内容
-        if (arg === 1) {
-          this.ipagination.current = 1;
-        }
-        let params = this.getQueryParams();//查询条件
-        this.loading = true;
-        getAction(this.url.list, params).then((res) => {
-          if (res.code===200) {
-            this.dataSource = res.data.rows;
-            this.ipagination.total = res.data.total;
-            this.tableAddTotalRow(this.columns, this.dataSource)
-            this.realityPriceTotal = res.data.realityPriceTotal
-          } else if(res.code===510){
-            this.$message.warning(res.data)
-          } else {
-            this.$message.warning(res.data.message)
-          }
-          this.loading = false;
-        })
-      },
-      initCustomer() {
-        let that = this;
-        findBySelectCus({}).then((res)=>{
-          if(res) {
-            that.cusList = res
-          }
-        });
-      },
-      getDepotData() {
-        getAction('/depot/findDepotByCurrentUser').then((res)=>{
-          if(res.code === 200){
-            this.depotList = res.data;
-          }else{
-            this.$message.info(res.data);
-          }
-        })
-      },
-      loadAllOrgaData(){
-        let that = this
-        let params = {}
-        getAllOrganizationTreeByUser(params).then((res)=>{
-          if(res){
-            that.orgaTree = res
-          }
-        })
-      },
       searchQuery() {
-        if(this.queryParam.beginTime == '' || this.queryParam.endTime == ''){
-          this.$message.warning('请选择单据日期！')
-        } else {
-          this.loadData(1);
-        }
+        this.loadData(1);
+      },
+      showAccountInOutList(record) {
+        this.$refs.accountInOutList.show(record);
+        this.$refs.accountInOutList.title = "查看账户流水-" + record.name;
+        this.$refs.accountInOutList.disableSubmit = false;
       },
       exportExcel() {
         let list = []
-        let head = '条码,名称,规格,型号,颜色,品牌,制造商,扩展信息,单位,销售数量,销售金额,退货数量,退货金额,实际销售金额'
+        let head = '名称,编号,期初金额,本月发生额,当前余额'
         for (let i = 0; i < this.dataSource.length; i++) {
           let item = []
           let ds = this.dataSource[i]
-          item.push(ds.barCode, ds.materialName, ds.materialStandard, ds.materialModel, ds.materialColor, ds.materialBrand,
-            ds.materialMfrs, ds.materialOther, ds.materialUnit, ds.outSum,
-            ds.outSumPrice, ds.inSum, ds.inSumPrice, ds.outInSumPrice)
+          item.push(ds.name, ds.serialNo, ds.initialAmount, ds.thisMonthAmount, ds.currentAmount)
           list.push(item)
         }
-        let tip = '单据日期：' + this.queryParam.beginTime + '~' + this.queryParam.endTime
-        this.handleExportXlsPost('销售统计', '销售统计', head, tip, list)
+        let tip = '账户统计查询'
+        this.handleExportXlsPost('账户统计', '账户统计', head, tip, list)
       }
     }
   }
